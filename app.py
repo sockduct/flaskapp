cdfec94d05778e0eb958a03826d662b1917bf158
James Small <james.r.small@outlook.com>
2018-12-09 12:20:55 -0500

    update:  Add routes, mock storage objects

--- app.py
+++ app.py
@@ -4,9 +4,32 @@ from flask import Flask
 
 
 app = Flask(__name__)
+users = {'test': ''}
+journals = {'Test': {'user': 'test',
+                     'text': 'This is a test journal entry.'}}
 
 
 @app.route('/')
 def main():
     return '<h1>Welcome to the Journal!</h1>'
+
+
+@app.route('/register')
+def register():
+    return '<h1>User Sign Up</h1>'
+
+
+@app.route('/login')
+def login():
+    return '<h1>User Login</h1>'
+
+
+@app.route('/logout')
+def logout():
+    return '<h1>User Logout</h1>'
+
+
+@app.route('/entry')
+def entry():
+    return '<h1>Create A Journal Entry</h1>'
 
