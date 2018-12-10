20d52f07b16f0c5b4f2d12d4b5157441fbdc1d28
James Small <james.r.small@outlook.com>
2018-12-10 13:15:00 -0500

    update:  Add functionality to securely load test user password

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
 #!/usr/bin/env python3
 
+from dotenv import load_dotenv
 from flask import Flask
+import os
+from werkzeug import check_password_hash, generate_password_hash
 
 
 app = Flask(__name__)
+# __file__ is the full pathname of this module
+basedir = os.path.abspath(os.path.dirname(__file__))
+# Load environment variables from this file
+load_dotenv(os.path.join(basedir, '.env'))
 users = {'test': ''}
+test_passwd = os.environ.get('TEST_PASSWD')
+users['test'] = test_passwd
 journals = {'Test': {'user': 'test',
                      'text': 'This is a test journal entry.'}}
 
